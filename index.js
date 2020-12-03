6f8d0718cec1fdf3c49bde75311821dd91fa03b9
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-12-03 09:31:46 -0500

    APOD Continued

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const express = require('express');
-const app = express();
+var http = require('http');
 const port = process.env.PORT || '3000';
-
 const api = require('./lib/index.js');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+const parameters = {
+	key: 'ofmobU4wrsnPlHaeSQUngj5xgofeEvZ8Hf0SE81X',
+	date: '2020-11-10',
+	hd: 'false'
+};
 
-app.get('/apod', function (response) {
-	const parameters = {
-		key: 'ofmobU4wrsnPlHaeSQUngj5xgofeEvZ8Hf0SE81X',
-		date: '2020-11-20',
-		hd: 'false'
-	};
-    api.apod(parameters, function(response) {
-		console.log(response);
-		return 200;
-    });
-});
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+http.createServer(function (req, res) {
+	const apiCall = api.apod(parameters, function(response) {
+  		return response;
+	});
+	console.log(apiCall)
+  	res.writeHead(200, {'Content-Type': 'text/html'});
+}).listen(port, () => {console.log(`Celestial Bodies is listening at http://localhost:${port}`)}); 
