482d4ee8eacd12ff8176a3543b5fcd5ffb5c8ca3
Jeffrey Brown <jeffreybrown@Jeffreys-MacBook-Pro.local>
2021-10-01 19:55:01 -0400

    Finalize asteroids function and linting

--- testing.js
+++ testing.js
@@ -1,14 +1,11 @@
-const celestial = require('./index');
+const celestial = require('./index')
 
-// get results for 04/20/2021 //
 const payload = {
-  end_date: '2021-04-27',
+  asteroid_id: '2138893',
   key: 'DEMO_KEY',
-  start_date: '2021-04-20',
-  type: 'feed'
-};
+  type: 'browse'
+}
 
 celestial.asteroids(payload, neows => {
-  console.log(neows)
   return neows;
-});
+})
